c30a37dad4548986ad215feff23e2f32e6d19fe2
jogaro1983 <jogaro1983@hotmail.es>
2022-12-03 20:02:17 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(){
+    let dato = "Hola React";
+    return (<div id="datoReact">{dato}</div>);
+
+}
---
+++ comunes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function returnDiv(){
+    let dato = "Hola React";
+    return (<div id="datoReact">${dato}</div>);
+
+}
